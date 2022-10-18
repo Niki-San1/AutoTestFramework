94dd12290b958a6b7be9bd81c9c59043f52d52e1
Nikola Titsov <niki.titsov@gmail.com>
2022-10-18 17:07:04 +0300

    Created driver property

--- AutoTestFramework/Driver.cs
+++ AutoTestFramework/Driver.cs
@@ -1,7 +1,9 @@
-﻿namespace AutoTestFramework
+﻿using OpenQA.Selenium;
+
+namespace AutoTestFramework
 {
     public static class Driver
     {
-
+        public static IWebDriver driver { get; set; }
     }
 }
