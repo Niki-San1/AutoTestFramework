b9046f5296136f9f1ceec037003a40d8f3980f25
Nikola Titsov <niki.titsov@gmail.com>
2022-10-18 20:15:14 +0300

    Created SelectorsPage Page Objects

--- AutoTestFramework/Pages/SelectorsPage.cs
+++ AutoTestFramework/Pages/SelectorsPage.cs
@@ -1,7 +1,28 @@
-﻿namespace AutoTestFramework.Pages
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace AutoTestFramework.Pages
 {
     public class SelectorsPage
     {
+        public SelectorsPage()
+        {
+            PageFactory.InitElements(Driver.driver, this);
+        }
 
+        [FindsBy(How = How.CssSelector, Using = "#main-content > article.mh-loop-item.mh-clearfix.post-111.post.type-post.status-publish.format-standard.has-post-thumbnail.hentry.category-selectors > div > header > h3 > a")]
+        public IWebElement Name { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "#main-content > article.mh-loop-item.mh-clearfix.post-110.post.type-post.status-publish.format-standard.has-post-thumbnail.hentry.category-selectors > div > header > h3 > a")]
+        public IWebElement Id { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "#main-content > article.mh-loop-item.mh-clearfix.post-109.post.type-post.status-publish.format-standard.has-post-thumbnail.hentry.category-selectors > div > header > h3 > a")]
+        public IWebElement XPath { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "#main-content > article.mh-loop-item.mh-clearfix.post-108.post.type-post.status-publish.format-standard.has-post-thumbnail.hentry.category-selectors > div > header > h3 > a")]
+        public IWebElement CSSPath { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "#main-content > article.mh-loop-item.mh-clearfix.post-107.post.type-post.status-publish.format-standard.has-post-thumbnail.hentry.category-selectors > div > header > h3 > a")]
+        public IWebElement ClassName { get; set; }
     }
 }
