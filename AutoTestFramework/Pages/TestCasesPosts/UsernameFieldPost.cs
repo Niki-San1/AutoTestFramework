053a909f3fbd4158bdac3453e315e5ec185b0003
Nikola Titsov <niki.titsov@gmail.com>
2022-10-18 20:47:31 +0300

    Created UsernameFieldPost Page Objects

--- AutoTestFramework/Pages/TestCasesPosts/UsernameFieldPost.cs
+++ AutoTestFramework/Pages/TestCasesPosts/UsernameFieldPost.cs
@@ -1,7 +1,19 @@
-﻿namespace AutoTestFramework.Pages.TestCasesPosts
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace AutoTestFramework.Pages.TestCasesPosts
 {
     public class UsernameFieldPost
     {
+        public UsernameFieldPost()
+        {
+            PageFactory.InitElements(Driver.driver, this);
+        }
 
+        [FindsBy(How = How.CssSelector, Using = "#post-74 > div > figure > img")]
+        public IWebElement Image { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "#post-74 > div > p > a")]
+        public IWebElement LoginFormLink { get; set; }
     }
 }
