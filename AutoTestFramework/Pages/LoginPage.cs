0e9069761666a41803be2776efda62b144544f27
Nikola Titsov <niki.titsov@gmail.com>
2022-10-18 19:59:39 +0300

    Created LoginPage Page Objects

--- AutoTestFramework/Pages/LoginPage.cs
+++ AutoTestFramework/Pages/LoginPage.cs
@@ -1,7 +1,26 @@
-﻿namespace AutoTestFramework.Pages
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace AutoTestFramework.Pages
 {
     public class LoginPage
     {
+        public LoginPage()
+        {
+            PageFactory.InitElements(Driver.driver, this);
+        }
 
+
+        [FindsBy(How = How.Name, Using = "userid")]
+        public IWebElement UsernameField { get; set; }
+
+        [FindsBy(How = How.Name, Using = "passid")]
+        public IWebElement PasswordField { get; set; }
+
+        [FindsBy(How = How.Name, Using = "repeatpassid")]
+        public IWebElement RepeatPasswordField { get; set; }
+
+        [FindsBy(How = How.Name, Using = "submit")]
+        public IWebElement LoginButton { get; set; }
     }
 }
