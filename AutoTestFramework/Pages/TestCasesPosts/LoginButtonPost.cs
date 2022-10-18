fce053fe919d7d8d8dcb195a93bb770e00e5a12a
Nikola Titsov <niki.titsov@gmail.com>
2022-10-18 20:51:55 +0300

    Created LoginButtonPost Page Objects

---
+++ AutoTestFramework/Pages/TestCasesPosts/LoginButtonPost.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace AutoTestFramework.Pages.TestCasesPosts
+{
+    public class LoginButtonPost
+    {
+        public LoginButtonPost()
+        {
+            PageFactory.InitElements(Driver.driver, this);
+        }
+
+        [FindsBy(How = How.Name, Using = "testButton")]
+        public IWebElement Button { get; set; }
+    }
+}
