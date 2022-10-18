97c53419655c4e4741a6cdde6238443213ee00d8
Nikola Titsov <niki.titsov@gmail.com>
2022-10-18 17:40:13 +0300

    Created Header Page Objects

---
+++ AutoTestFramework/Pages/PageElements/Header.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace AutoTestFramework.Pages.PageElements
+{
+    internal class Header
+    {
+        public Header()
+        {
+            PageFactory.InitElements(Driver.driver, this);
+        }
+
+        [FindsBy(How = How.ClassName, Using = "mh-header-title")]
+        public IWebElement Headline { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "mh-header-tagline")]
+        public IWebElement SubHeadline { get; set; }
+    }
+}
