438971c5e0041cd6aa06c04cc8c81429da57a77f
Nikola Titsov <niki.titsov@gmail.com>
2022-11-01 19:30:28 +0200

    Created NavigateTo class

--- AutoTestFramework/NavigateTo.cs
+++ AutoTestFramework/NavigateTo.cs
@@ -1,7 +1,35 @@
-﻿namespace AutoTestFramework
+﻿using AutoTestFramework.Pages;
+using AutoTestFramework.Pages.PageElements;
+
+namespace AutoTestFramework
 {
     public static class NavigateTo
     {
+        public static void LoginPost()
+        {
+            Menu menu = new Menu();
+            menu.TestScenarios.Click();
 
+            TestScenariosPage TSPage = new TestScenariosPage();
+            TSPage.LoginForm.Click();
+        }
+
+        public static void RegisterPost()
+        {
+            Menu menu = new Menu();
+            menu.TestScenarios.Click();
+
+            TestScenariosPage TSPage = new TestScenariosPage();
+            TSPage.RegisterForm.Click();
+        }
+
+        public static void LoginButtonPost()
+        {
+            Menu menu = new Menu();
+            menu.TestCases.Click();
+
+            TestCasesPage TCPage = new TestCasesPage();
+            TCPage.LoginButtonInLoginForm.Click();
+        }
     }
 }
