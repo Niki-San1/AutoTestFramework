70831b13ff010a29eb95e8a250fd675ef4ca65b2
Nikola Titsov <niki.titsov@gmail.com>
2022-11-01 20:25:46 +0200

    Created LoginValid test.

--- AutoTestFramework/Tests/Cases/LoginValid.cs
+++ AutoTestFramework/Tests/Cases/LoginValid.cs
@@ -1,7 +1,36 @@
-﻿namespace AutoTestFramework.Tests.Cases
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+
+namespace AutoTestFramework.Tests.Cases
 {
     public class LoginValid
     {
+        IAlert Alert;
 
+        [OneTimeSetUp]
+        public void Initialize()
+        {
+            Actions.InitializeDriver();
+            NavigateTo.LoginPost();
+        }
+
+        [Test]
+        public void ValidLogIn()
+        {
+            Actions.FillLogInForm(Config.Credentials.Valid.UserID, Config.Credentials.Valid.PasswordLogin, Config.Credentials.Valid.RepeatPassword);
+
+            Alert = Driver.driver.SwitchTo().Alert();
+
+            Assert.AreEqual(Config.AlertBoxMessages.LoginSuccessful, Alert.Text);
+
+            Alert.Accept();
+        }
+
+        [OneTimeTearDown]
+        public void CleanUp()
+        {
+            Driver.driver.Quit();
+        }
     }
 }
